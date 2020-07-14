4cf3695b1a960b0cfa91376543fbe99c4b54cd86
andreevnanka <65194176+andreevnanka@users.noreply.github.com>
2020-07-14 23:52:59 +0300

    Внесены правки по результатам первой автоматической проверки

--- js/feedback.js
+++ js/feedback.js
@@ -1,10 +1,10 @@
 var feedbackLink = document.querySelector(".feedback-button");
 var feedbackPopup = document.querySelector(".modal-feedback-reg");
-var feedbackClose = document.querySelector(".body");
-var feedbackLogin = FeedbackPopup.querySelector(".mod2-username");
-var feedbackMail = FeedbackPopup.querySelector(".mod2-usermail");
+var feedbackClose = FeedbackPopup.querySelector(".feedback-close");
+var feedbackLogin = FeedbackPopup.querySelector(".feedback-username");
+var feedbackMail = FeedbackPopup.querySelector(".feedback-usermail");
 var feedbackForm = FeedbackPopup.querySelector(".feedback-form");
-var feedbackComment = FeedbackPopup.querySelector(".mod2-comment");
+var feedbackComment = FeedbackPopup.querySelector(".feedback-comment");
 
 feedbackLink.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -17,27 +17,24 @@ feedbackLink.addEventListener("click", function (evt) {
   }
   if (storage) {
     feedbackMail.value = storage;
-    FeedbackComment.focus();
+    feedbackComment.focus();
   } else {
     feedbackLogin.focus();
     feedbackMail.focus();
   }
 });
-
 feedbackClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.remove("modal-show");
   feedbackPopup.classList.remove("modal-error");
 });
-
-FeedbackForm.addEventListener("submit", function (evt) {
-  if (!FeedbackLogin.value || !FeedbackMail.value || !FeedbackComment.value) {
+feedbackForm.addEventListener("submit", function (evt) {
+  if (!feedbackLogin.value || !feedbackMail.value || !feedbackComment.value) {
     evt.preventDefault();
     feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
     feedbackPopup.classList.add("modal-error");
   }
 });
-
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (feedbackPopup.classList.contains("modal-show")) {
